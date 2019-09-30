d27683ddb37506a72dfdaf7c7d6f01fa97a6da12
Evgeniy Lagun <lagun.evgeniy30@gmail.com>
2019-09-30 22:39:43 +0300

    my solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let message = '';
+    let pos = 0;
+    while (pos < expr.length){
+        if (expr[pos] == '*') {
+            message += ' ';
+            pos += 10;
+            continue;
+        }
+        let current_letter = expr.slice(pos, pos + 10);
+        let morse_code = '';
+        for (let j = 0; j < current_letter.length; j++){
+            if ((current_letter[j] == '0') && (j % 2 == 0)) {
+                j++;
+            } else if ((current_letter[j] == '1') && (current_letter[j + 1] == '1')) {
+                morse_code += '-';
+                j++;
+            } else {
+                morse_code += '.';
+                j++
+            }
+        }
+        for (let [code, symbol] of Object.entries(MORSE_TABLE)) {
+            if (code == morse_code) {
+                message += symbol;
+                break;
+            }
+        }
+        pos += 10;
+    }
+    return message;
 }
 
 module.exports = {
